bbe631216cea5d860a5a3c15eb270fa1ff70cd21
amrit <mr.amrrit7@gmail.com>
2022-07-11 13:24:04 +0530

    added model for todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import InputField from "./components/InputField";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo);
   return (
     <div className="App">
       <h1 className="heading">React</h1>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Input, Button, Stack } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const InputField: React.FC = () => {
-  return <div>InputField</div>;
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: "#11cb5f",
+    },
+  },
+});
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+  return (
+    <form>
+      <Stack spacing={2} direction="row">
+        <ThemeProvider theme={theme}>
+          <Input
+            id="my-input"
+            aria-describedby="my-helper-text"
+            placeholder="Enter todo"
+            color="primary"
+            value={todo}
+            onChange={e => setTodo(e.target.value)}
+          />
+          <Button color="primary">ADD</Button>
+        </ThemeProvider>
+      </Stack>
+    </form>
+  );
 };
 
 export default InputField;
