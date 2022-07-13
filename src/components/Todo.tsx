57fa1ee516f07af191b84ff835907cd9f93b11ea
amrit <mr.amrrit7@gmail.com>
2022-07-13 16:08:18 +0530

    feat: edit functionalty

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import DoneIcon from "@mui/icons-material/Done";
-import { ListItem, ListItemText, IconButton } from "@mui/material";
+import { ListItem, ListItemText, IconButton, TextField } from "@mui/material";
 import { TodoModel } from "../Model";
 
 type Props = {
@@ -29,21 +29,42 @@ const Todo = ({ todo, setTodos, todos }: Props) => {
     }
   };
 
+  const updateHandler = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(prevTodos => {
+      return prevTodos.map(todo =>
+        todo.id === id ? { ...todo, todo: editTodo } : todo
+      );
+    });
+    setEdit(!edit);
+  };
+
   const removeTodo = (id: number) => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
 
   return (
-    <div>
+    <form onSubmit={e => updateHandler(e, todo.id)}>
       <ListItem>
-        <ListItemText
-          style={{
-            color: "#f1f1ef",
-            width: "30rem",
-            textDecoration: `${todo.isDone ? "line-through" : "none"}`,
-          }}
-          primary={todo.todo}
-        />
+        {edit ? (
+          <TextField
+            type="string"
+            value={editTodo}
+            variant="filled"
+            style={{ width: "30rem" }}
+            onChange={e => setEditTodo(e.target.value)}
+          />
+        ) : (
+          <ListItemText
+            style={{
+              color: "#f1f1ef",
+              width: "30rem",
+              textDecoration: `${todo.isDone ? "line-through" : "none"}`,
+            }}
+            primary={todo.todo}
+          />
+        )}
 
         <IconButton onClick={() => markDone(todo.id)}>
           <DoneIcon />
@@ -55,7 +76,7 @@ const Todo = ({ todo, setTodos, todos }: Props) => {
           <DeleteIcon />
         </IconButton>
       </ListItem>
-    </div>
+    </form>
   );
 };
 
